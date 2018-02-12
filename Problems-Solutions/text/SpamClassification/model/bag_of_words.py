7e2c72a3b80cc489adedebd6f9daf14c65798571
kishorekarunakarank@gmail.com <kishorekarunakarank@gmail>
2018-02-12 16:46:39 +0530

    Adding f1 scorre to the model
    Updating estimator with f1-score, both micro and macro average is supported. Refer https://sebastianraschka.com/faq/docs/multiclass-metric.html and
    http://rushdishams.blogspot.in/2011/08/micro-and-macro-average-of-precision.html

--- Problems-Solutions/text/SpamClassification/model/bag_of_words.py
+++ Problems-Solutions/text/SpamClassification/model/bag_of_words.py
@@ -1,11 +1,12 @@
-__author__ = 'KKishore'
-
 import multiprocessing
+from itertools import chain
 
 import tensorflow as tf
 from tensorflow.contrib import lookup
 
 from model import commons
+
+__author__ = 'KKishore'
 
 
 def parse_record(record):
@@ -39,6 +40,47 @@ def input_fn(file_name, shuffle=False, batch_size=16, repeat_count=1):
     iterator = data_set.make_one_shot_iterator()
     features, label = iterator.get_next()
     return features, decode_target(label)
+
+
+def streaming_f1(labels, predictions, n_classes, weights=None, type='macro'):
+    '''
+    Refer http://rushdishams.blogspot.in/2011/08/micro-and-macro-average-of-precision.html
+    for micro and macro f1
+    :param labels:
+    :param predictions:
+    :param n_classes:
+    :param type:
+    :return:
+    '''
+    labels_and_predictions_by_class = [(tf.equal(labels, c), tf.equal(predictions, c)) for c in range(0, n_classes)]
+    tp_by_class_val, tp_by_class_update_op = zip(*[tf.metrics.true_positives(label, prediction, weights=weights) for label, prediction in
+                                                   labels_and_predictions_by_class])
+    fn_by_class_val, fn_by_class_update_op = zip(*[tf.metrics.false_negatives(label, prediction, weights=weights) for label, prediction
+                                                   in labels_and_predictions_by_class])
+    fp_by_class_val, fp_by_class_update_op = zip(*[tf.metrics.false_positives(label, prediction, weights=weights) for label, prediction
+                                                   in labels_and_predictions_by_class])
+
+    f1_update_op = tf.group(*chain(tp_by_class_update_op, fn_by_class_update_op, fp_by_class_update_op))
+
+    if type == 'macro':
+        epsilon = [10e-6 for _ in range(n_classes)]
+
+        f1_val = tf.multiply(2., tp_by_class_val) / (tf.reduce_sum([tf.multiply(2., tp_by_class_val),
+                                                                    fp_by_class_val, fn_by_class_val, epsilon],
+                                                                   axis=0))
+        f1_val = tf.reduce_mean(f1_val)
+    else:
+        epsilon = 10e-6
+
+        total_tp = tf.reduce_sum(tp_by_class_val)
+        total_fn = tf.reduce_sum(fn_by_class_val)
+        total_fp = tf.reduce_sum(fp_by_class_val)
+
+        f1_val = tf.squeeze(tf.multiply(2., total_tp) / (tf.multiply(2., total_tp) +
+                                                         total_fp + total_fn + epsilon,
+                                                         ))
+
+    return f1_val, f1_update_op
 
 
 def model_fn(features, labels, mode, params):
@@ -94,7 +136,11 @@ def model_fn(features, labels, mode, params):
 
     if mode == tf.estimator.ModeKeys.EVAL:
         eval_metrics_ops = {
-            'accuracy': tf.metrics.accuracy(labels=labels, predictions=prediction_indices, weights=weights)
+            'accuracy': tf.metrics.accuracy(labels=labels, predictions=prediction_indices, weights=weights),
+            'precision': tf.metrics.precision(labels=labels, predictions=prediction_indices, weights=weights),
+            'recall': tf.metrics.recall(labels=labels, predictions=prediction_indices, weights=weights),
+            'f1_score': streaming_f1(labels=labels, predictions=prediction_indices, n_classes=commons.TARGET_SIZE,
+                                     weights=None)
         }
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metrics_ops)
 
